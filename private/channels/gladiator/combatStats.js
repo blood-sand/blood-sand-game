8ebcc05916f4509d0dc857d8fcde5d1519156c80
Spencer A. Lockhart <slockhartbiz@gmail.com>
2019-09-14 06:58:34 -0600

    generate combat stats every request

--- private/channels/gladiator/combatStats.js
+++ private/channels/gladiator/combatStats.js
@@ -1,22 +1,5 @@
 const jsonSL = require('json-sl');
-/*
-const sexes = {
-    male: 0,
-    female: 1
-};
-const cultures = {
-    roman: 0,
-    gallic: 1,
-    germanic: 2,
-    syrian: 3,
-    numidian: 4,
-    thracian: 5,
-    greek: 6,
-    iberian: 7,
-    judean: 8,
-    scythian: 9
-};
-*/
+
 module.exports = function (m, local) {
     const combatStatsGenerator = require('../../jsonSL/combat_stats.json');
 	const socket = local.socket;
@@ -43,11 +26,7 @@ module.exports = function (m, local) {
         console.log(session.combatStats);
         socket.emit("gladiator-combatStats", session.combatStats);
     }
-    if (session.combatStats) {
-        socket.emit("gladiator-combatStats", session.combatStats);
-    } else {
-        generateCombatStats();
-    }
+    generateCombatStats();
     
     socket.on('gladiator-combatStats-generate', generateCombatStats);
 	
