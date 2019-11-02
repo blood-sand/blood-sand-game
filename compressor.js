4c23e8d40102273df96a8effa64e26669d26b80e
Spencer A. Lockhart <slockhartbiz@gmail.com>
2019-11-01 20:19:09 -0600

    Moved module compressor to hookmod.

--- compressor.js
+++ compressor.js
@@ -1,3 +1,9 @@
 // Compressor
-require('./moduleCompressor.js');
+let Hookmod = require('hookmod');
+
+let hm = new Hookmod('src/module');
+
+hm.writeTo('src/js/compressedModules.js');
+console.log("Modules compressed.");
+
 require('./scriptCompressor.js');
